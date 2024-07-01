38e47fe8ce46062327a4ec6997eaec65308ef11b
McFlanky <cturjanica02@gmail.com>
2024-07-01 11:30:38 -0400

    Finished grpc transport for calling node and added version, handshake and ack messages with protobuffers

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"github.com/McFlanky/blocker/node"
 	"github.com/McFlanky/blocker/proto"
@@ -12,7 +14,6 @@ import (
 
 func main() {
 	node := node.NewNode()
-
 	opts := []grpc.ServerOption{}
 	grpcServer := grpc.NewServer(opts...)
 	ln, err := net.Listen("tcp", ":3000")
@@ -21,5 +22,32 @@ func main() {
 	}
 	proto.RegisterNodeServer(grpcServer, node)
 	fmt.Println("node running on port", ":3000")
+
+	go func() {
+		for {
+			time.Sleep(2 * time.Second)
+			makeTransaction()
+		}
+	}()
+
 	grpcServer.Serve(ln)
+}
+
+func makeTransaction() {
+	client, err := grpc.NewClient(":3000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := proto.NewNodeClient(client)
+
+	version := &proto.Version{
+		Version: "blockey v0.1",
+		Height:  1,
+	}
+
+	_, err = c.Handshake(context.TODO(), version)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- node/node.go
+++ node/node.go
@@ -2,18 +2,35 @@ package node
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/McFlanky/blocker/proto"
+	"google.golang.org/grpc/peer"
 )
 
 type Node struct {
+	version string
 	proto.UnimplementedNodeServer
 }
 
 func NewNode() *Node {
-	return &Node{}
+	return &Node{
+		version: "blockey v0.1",
+	}
 }
 
-func (n *Node) HandleTransaction(ctx context.Context, tx *proto.Transaction) (*proto.None, error) {
-	return nil, nil
+func (n *Node) Handshake(ctx context.Context, v *proto.Version) (*proto.Version, error) {
+	ourVersion := &proto.Version{
+		Version: n.version,
+		Height:  100,
+	}
+	peer, _ := peer.FromContext(ctx)
+	fmt.Printf("received version from %s: %+v\n", v, peer.Addr)
+	return ourVersion, nil
+}
+
+func (n *Node) HandleTransaction(ctx context.Context, tx *proto.Transaction) (*proto.Ack, error) {
+	peer, _ := peer.FromContext(ctx)
+	fmt.Println("received transaction from:", peer)
+	return &proto.Ack{}, nil
 }
