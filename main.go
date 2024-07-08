3fb1f663433b21582744eba1113884c8ce492eae
McFlanky <cturjanica02@gmail.com>
2024-07-07 21:34:51 -0400

    Added peer discovery with custom gossip protocol with new node on port :5000, and more abstracted logic with bootstrapNetwork, new functions: {canConnectWith, getPeerList} plus added new logic to add peer to see if peer is not connecting with its alreadyu same port and to make sure they all run in goroutines and not runnnig into mutex errors

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/McFlanky/blocker/node"
 	"github.com/McFlanky/blocker/proto"
@@ -11,26 +12,17 @@ import (
 
 func main() {
 	makeNode(":3000", []string{})
+	time.Sleep(time.Second)
 	makeNode(":4000", []string{":3000"})
-
-	// go func() {
-	// 	for {
-	// 		time.Sleep(2 * time.Second)
-	// 		makeTransaction()
-	// 	}
-	// }()
+	time.Sleep(4 * time.Second)
+	makeNode(":5000", []string{":4000"})
 
 	select {}
 }
 
 func makeNode(listenAddr string, bootstrapNodes []string) *node.Node {
 	n := node.NewNode()
-	go n.Start(listenAddr)
-	if len(bootstrapNodes) > 0 {
-		if err := n.BootstrapNetwork(bootstrapNodes); err != nil {
-			log.Fatal(err)
-		}
-	}
+	go n.Start(listenAddr, bootstrapNodes)
 	return n
 }
 
