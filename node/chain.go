cfa64c1d144b8b6dd96cd6d6f31e8534d176eddf
McFlanky <cturjanica02@gmail.com>
2024-07-10 17:17:40 -0400

    Added features to store blocks, fetch by height/hash, add blocks and add/get/height/length of headers , and testing for chain height, adding block as well as in memory store for block storing

---
+++ node/chain.go
@@ -0,0 +1,76 @@
+package node
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/McFlanky/blocker/proto"
+	"github.com/McFlanky/blocker/types"
+)
+
+type HeaderList struct {
+	headers []*proto.Header
+}
+
+func NewHeaderList() *HeaderList {
+	return &HeaderList{
+		headers: []*proto.Header{},
+	}
+}
+
+func (list *HeaderList) Add(h *proto.Header) {
+	list.headers = append(list.headers, h)
+}
+
+func (list *HeaderList) Get(index int) *proto.Header {
+	if index > list.Height() {
+		panic("index too high!")
+	}
+	return list.headers[index]
+}
+
+func (list *HeaderList) Height() int {
+	return list.Len() - 1
+}
+
+// [A, B, C, D, E] len = 5, height = 4
+func (list *HeaderList) Len() int {
+	return len(list.headers)
+}
+
+type Chain struct {
+	blockstore BlockStorer
+	headers    *HeaderList
+}
+
+func NewChain(bs BlockStorer) *Chain {
+	return &Chain{
+		blockstore: bs,
+		headers:    NewHeaderList(),
+	}
+}
+
+func (c *Chain) Height() int {
+	return c.headers.Height()
+}
+
+func (c *Chain) AddBlock(b *proto.Block) error {
+	// Add the header to the list of headers
+	c.headers.Add(b.Header)
+	// validation
+	return c.blockstore.Put(b)
+}
+
+func (c *Chain) GetBlockByHash(hash []byte) (*proto.Block, error) {
+	hashHex := hex.EncodeToString(hash)
+	return c.blockstore.Get(hashHex)
+}
+
+func (c *Chain) GetBlockByHeight(height int) (*proto.Block, error) {
+	if c.Height() < height {
+		return nil, fmt.Errorf("given height [%d] too high - height [%d]", height, c.Height())
+	}
+	header := c.headers.Get(height)
+	hash := (types.HashHeader(header))
+	return c.GetBlockByHash(hash)
+}
